082db54a632cdaaf7ebf3a25d1d085bb15ebe8c5
Abdulakeem Abdulazeez Aramide <oyeadeof@gmail.com>
2026-03-30 09:57:39 +0100

    Debugging Database Setup

--- core/settings.py
+++ core/settings.py
@@ -10,9 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
-from pathlib import Path
-import dj_database_url
 import os
+import dj_database_url
+from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -83,12 +83,27 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600, # Keeps connections alive for better performance
+            conn_health_checks=True,
+        )
     }
-}
+    print("✅ Django is using the Remote PostgreSQL Database")
+else:
+    # 3. Fallback to local SQLite for your local development
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+    print("⚠️ DATABASE_URL not found, Django is using local SQLite")
+
 
 DATABASES = {
     'default': dj_database_url.config(
